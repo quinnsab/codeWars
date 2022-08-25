b11a0e38d871b0b7eb28bf95727002506c612b39
Erin Sheridan <erinsabs@gmail.com>
2022-08-25 17:26:12 -0400

    5 string codewars HW

---
+++ Remove spaces from string.js
@@ -0,0 +1,6 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+// https://losseff.xyz/katas/007-remove-string-spaces/javascript/
+function noSpace(x){
+  return x.split(" ").join('')
+}
---
+++ Reverse string.js
@@ -0,0 +1,8 @@
+// Complete the solution so that it reverses the string passed into it.
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+ const reverseStr = str.split('').reverse('').join('');
+ return reverseStr;
+}
